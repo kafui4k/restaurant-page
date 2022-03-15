a733a8a79db29ac2fe1e0fad884edcd4b274a0cb
Kafui Alordo <kalordo7@gmail.com>
2022-03-15 22:38:54 +0000

    Create bare-bones homepage layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('restaurant page project setup!');
+// const content = document.querySelector('#content');
+
+// const header = document.createElement('div');
+// header.classList.add('header');
+// header.innerHTML = `
+//     <nav>
+//         <ul>
+//             <li>About</li>
+//             <li>Menu</li>
+//             <li>contact</li>
+//         </ul>
+//     </nav>`;
+
+// content.appendChild(header);
+
